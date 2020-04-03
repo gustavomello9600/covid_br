bebab5f7503840c473913b1e1aa3edd574979cda
gustavomello9600 <gustavomelloenciv@gmail.com>
2020-04-03 09:10:43 -0300

    Documentação adicionada e sufixos das bases de dados aprimorados

--- covidbr_leitoslimitados.py
+++ covidbr_leitoslimitados.py
@@ -20,14 +20,16 @@ class F():
     def __radd__(self, other):
         self.__add__(self, other)
 
+def número_de(f):
+    return f.value
 
 #Dados experimentais
 alfa = 8.414499908
 beta = 0.1969820003
 gama = -15.31551146
-mort = 1.305/100 #livre para variar
-R0 = 2.79 #livre para variar
 S0 = 210000000
+mort = 0.823/100 #livre para variar
+R0 = 2.79 #livre para variar
 
 #Constantes
 M0 = alfa + gama
@@ -36,27 +38,31 @@ fat = mort * (beta/(R0 - 1))
 kr = (1 - mort) * (beta/(R0 - 1))
 I0 = alfa*beta/fat
 
+#Realidade dos leitos brasileiros
+taxa_de_hospitalização = 2.67/100
+total_de_leitos = 42630
+
 #Estado Inicial
 I = F(I0)
 R = F(((1-mort)/mort) * M0)
 M = F(M0)
 S = F(S0 - I.value - R.value - M.value)
 
-#Operador diferencial com relação ao tempo
-
 def d_dt(f):
+    "Operador diferencial com relação ao tempo"
     if f == S:
-        df = -kc * S.value * I.value
+        df_dt = -kc * S.value * I.value
     elif f == I:
-        df = kc * S.value * I.value - kr*I.value - fat*I.value
+        df_dt = kc * S.value * I.value - kr*I.value - fat*I.value
     elif f == R:
-        df = kr*I.value
+        df_dt = kr*I.value
     else:
-        df = fat*I.value
-    return df
+        df_dt = fat*I.value
+    return df_dt
 
-sufixo = "_R2-79_mort0-01305_limitedeleitos"
-step_division = 100000
+sufixo = "_R{}_mort{}_{}leitos".format(*["{:.2f}".format(n).replace(".", "-") for n in (R0, 100*mort)],total_de_leitos)
+total_de_mortos_sem_leito = 0
+step_division = 500000
 dt = 1/step_division
 table = []
 dias = 15
@@ -92,16 +98,20 @@ while I.value > 1:                                                              
         for f in S, I, R, M:
             f.value = new[f]
 
-    if I.value * 0.0267 > 42630:
-        Recuperariam = R.value - R_ant #Número de pessoas que se recuperariam
+    if número_de(I) * taxa_de_hospitalização > total_de_leitos: #Roda se não houver leitos suficiente
+        Recuperariam = R.value - R_ant #Número de pessoas que se recuperariam se houvessem leitos suficientes
         if Recuperariam > 42360/0.0267:
-            Recuperam = (1 - 0.0267)*Recuperariam + 42360 #Número de pessoas que se recuperaram
+            #Limita o número de recuperados dentre os que precisariam ser internados ao número de internações possíveis
+            Recuperam = (1 - 0.0267)*Recuperariam + 42360 #Número de pessoas que se recuperaram (usando os leitos)
             M_semleito = Recuperariam - Recuperam
+            total_de_mortos_sem_leito += M_semleito
             print("No dia {}, morreram {:.0f} pessoas sem leito".format(d+1, M_semleito))
             R.value = Recuperam + R_ant
             M.value += M_semleito
 
 
-#
+print("{} pessoas morreram sem leito".format(total_de_mortos_sem_leito))
+
+#Exporta os dados como um arquivo .csv
 data = pd.DataFrame(table, index=list(range(d+1)), columns=list("SIRM"))
 data.to_csv(os.getcwd() + "\\output" + sufixo + ".csv")
